05e9b0dc0a5233afa3111bdd121b97aa77a4eeda
Albéric Trancart <alberic@trancart.net>
2021-12-03 18:04:26 +0100

    Add modal to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Dialog from "@reach/dialog";
+import type { NextPage } from "next";
+import Head from "next/head";
+import { Modal } from "../components/ui/Modal/Modal";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>a11yinyerface</title>
-        <meta name="description" content="A worst-practice accessibility experiment" />
+        <meta
+          name="description"
+          content="A worst-practice accessibility experiment"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       a11yinyerface
+      <Modal isOpen>Welcome to a11yinyerface!</Modal>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
