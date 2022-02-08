bf3c540c39d4400e5d0d1c5b7cc9d5fc9665caae
Mamadou Ouologuem <mamadou.ouologuem02@gmail.com>
2022-02-08 20:53:34 +0100

    🚨 Run linter

--- components/ui/Modal/Modal.tsx
+++ components/ui/Modal/Modal.tsx
@@ -1,22 +1,22 @@
-import Dialog from "@reach/dialog";
-import { ReactNode } from "react";
-import "@reach/dialog/styles.css";
-import styles from "./Modal.module.css";
-import VisuallyHidden from "@reach/visually-hidden";
+import Dialog from '@reach/dialog';
+import { ReactNode } from 'react';
+import '@reach/dialog/styles.css';
+import styles from './Modal.module.css';
+import VisuallyHidden from '@reach/visually-hidden';
 
 interface Props {
-  children: ReactNode;
-  isOpen: boolean;
-  onClose?: () => void;
+	children: ReactNode;
+	isOpen: boolean;
+	onClose?: () => void;
 }
 
 export const Modal: React.FC<Props> = ({ children, isOpen, onClose }) => (
-  <Dialog isOpen={isOpen} onDismiss={onClose} className={styles.modal}>
-    <button className={styles.closeButton} onClick={onClose}>
-      {" "}
-      <VisuallyHidden>Close</VisuallyHidden> <span aria-hidden>×</span>{" "}
-    </button>
+	<Dialog isOpen={isOpen} onDismiss={onClose} className={styles.modal}>
+		<button className={styles.closeButton} onClick={onClose}>
+			{' '}
+			<VisuallyHidden>Close</VisuallyHidden> <span aria-hidden>×</span>{' '}
+		</button>
 
-    {children}
-  </Dialog>
+		{children}
+	</Dialog>
 );
