4987cfc1b52a14bfb6407b2aa9114c39e891a68c
Mamadou Ouologuem <mamadou.ouologuem02@gmail.com>
2022-02-15 21:26:31 +0100

    🔧 Configure Mui with Next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
+// Imported from https://github.com/mui/material-ui/blob/5b0d0c343c9b195e7328cc20461c9adc1f5ac02d/examples/nextjs-with-typescript/pages/_app.tsx
 import '../styles/globals.css';
-import type { AppProps } from 'next/app';
+import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import theme from 'src/theming/theme';
+import createEmotionCache from 'src/theming/createEmotionCache';
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+	emotionCache?: EmotionCache;
 }
 
-export default MyApp;
+export default function MyApp(props: MyAppProps) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+	return (
+		<CacheProvider value={emotionCache}>
+			<Head>
+				<title>a11yinyerface</title>
+				<meta
+					name='description'
+					content='A worst-practice accessibility experiment'
+				/>
+				<meta name='viewport' content='initial-scale=1, width=device-width' />
+			</Head>
+			<ThemeProvider theme={theme}>
+				{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+				<CssBaseline />
+				<Component {...pageProps} />
+			</ThemeProvider>
+		</CacheProvider>
+	);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,8 @@
+import { Container } from '@mui/material';
 import type { NextPage } from 'next';
-import Head from 'next/head';
 
 const Home: NextPage = () => {
-	return (
-		<>
-			<Head>
-				<title>a11yinyerface</title>
-				<meta
-					name='description'
-					content='A worst-practice accessibility experiment'
-				/>
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			a11yinyerface
-		</>
-	);
+	return <Container>a11yinyerface</Container>;
 };
 
 export default Home;
