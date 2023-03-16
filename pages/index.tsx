d12bad28a3dfbc4c597f8bce337ba9b8e8d1894d
Loona Riviere <loonar@sipios.com>
2023-03-16 18:03:04 +0100

    rework first page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
-import { Container } from '@mui/material';
+import { Stack, Container, Typography, Fab, Link } from '@mui/material';
 import type { NextPage } from 'next';
+import SendIcon from '@mui/icons-material/Send';
+import AppBar from 'components/ui/AppBar/AppBar';
 
 const Home: NextPage = () => {
-	return <Container>a11yinyerface</Container>;
+	return (
+		<>
+			<AppBar />
+			<Container>
+				<Stack
+					py={3}
+					spacing={4}
+					textAlign='center'
+					height='100vh'
+					justifyContent='center'
+				>
+					<Typography variant='h1'>
+						Welcome to <br /> a11y inyerface
+					</Typography>
+
+					<Link href='/fruits'>
+						<Fab color='secondary' variant='extended' size='large'>
+							Start <SendIcon sx={{ ml: 2 }} />
+						</Fab>
+					</Link>
+				</Stack>
+			</Container>
+		</>
+	);
 };
 
 export default Home;
--- src/theming/theme.tsx
+++ src/theming/theme.tsx
@@ -1,17 +1,14 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+import { teal, blueGrey } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
 	palette: {
 		primary: {
-			main: '#556cd6',
+			main: teal[600],
 		},
 		secondary: {
-			main: '#19857b',
-		},
-		error: {
-			main: red.A400,
+			main: blueGrey[700],
 		},
 	},
 });
