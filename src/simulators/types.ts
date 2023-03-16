dd9fef774fce565de4621c6d123c5c75630ef747
Loona Riviere <loonar@sipios.com>
2023-03-16 17:47:10 +0100

    add simulators and color filters

--- src/simulators/types.ts
+++ src/simulators/types.ts
@@ -1,8 +1,20 @@
 export enum SIMULATE_EFFECT {
 	ZOOM = 'zoom',
 	HIGHT_CONTRAST_FILTER = 'hightContrastFilter',
+	BLACK_GREY_FILTER = 'blackGreyFilter',
 	LIGHT_BLUR_FILTER = 'lightBlurFilter',
 	HEAVY_BLUR_FILTER = 'heavyBlurFilter',
 	DRUNKEN_MOUSE = 'drunkenMouse',
 	SCRAMBLE_LETTER = 'scrambleLetter',
+}
+
+export enum COLOR_FILTER_ID {
+	PROTANOPIA = 'protanopia', // All yellow
+	PROTANOMALY = 'protanomaly',
+	DEUTERANOPIA = 'deuteranopia',
+	DEUTERANOMALY = 'deuteranomaly',
+	TRITANOPIA = 'tritanopia',
+	TRITANOMALY = 'tritanomaly',
+	ACHROMATOPSIA = 'achromatopsia', // All grey
+	ACHROMATOMALY = 'achromatomaly',
 }
