28df04dd355d530fdbd3feee63fa1810c20b4759
RobinCar <robinc@theodo.fr>
2023-04-06 11:49:09 +0200

    feat: add distractions simulator

--- src/simulators/index.ts
+++ src/simulators/index.ts
@@ -1,70 +1,75 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 import {
-	activeHeavyBlurFilter,
-	activeLightBlurFilter,
-	removeBlurFilter,
-} from './blurFilters';
+  activeHeavyBlurFilter,
+  activeLightBlurFilter,
+  removeBlurFilter,
+} from "./blurFilters";
 import {
-	activeBlackAndGreyFilter,
-	activeColorFilterById,
-	activeHighContrastFilter,
-	removeColorFilter,
-} from './colorFilters';
-import { activeDrunkenMouse, removeDrunkenMouse } from './drunkenMouse';
-import { activeScrambleLetter, removeScrambleLetter } from './scrambleLetter';
-import { COLOR_FILTER_ID, SIMULATE_EFFECT } from './types';
-import { activeFullZoom, resetZoom } from './zoom';
+  activeBlackAndGreyFilter,
+  activeColorFilterById,
+  activeHighContrastFilter,
+  removeColorFilter,
+} from "./colorFilters";
+import { activeDistractions, removeDistractions } from "./distractions";
+import { activeDrunkenMouse, removeDrunkenMouse } from "./drunkenMouse";
+import { activeScrambleLetter, removeScrambleLetter } from "./scrambleLetter";
+import { COLOR_FILTER_ID, SIMULATE_EFFECT } from "./types";
+import { activeFullZoom, resetZoom } from "./zoom";
 
 export const useSimulator = (effects: SIMULATE_EFFECT[]) => {
-	useEffect(() => {
-		activateSimulators(effects);
+  useEffect(() => {
+    activateSimulators(effects);
 
-		return () => removeSimulations();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, effects);
+    return () => removeSimulations();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, effects);
 };
 
 export const useColorFilterSimulator = (filterIds: COLOR_FILTER_ID[]) => {
-	useEffect(() => {
-		filterIds.forEach((filterId) => activeColorFilterById(filterId));
+  useEffect(() => {
+    filterIds.forEach((filterId) => activeColorFilterById(filterId));
 
-		return () => removeColorFilter();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, filterIds);
+    return () => removeColorFilter();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, filterIds);
 };
 
 export const activateSimulators = (effects: SIMULATE_EFFECT[]) => {
-	effects.forEach((effect) => {
-		switch (effect) {
-			case SIMULATE_EFFECT.ZOOM:
-				activeFullZoom();
-				break;
-			case SIMULATE_EFFECT.HIGHT_CONTRAST_FILTER:
-				activeHighContrastFilter();
-				break;
-			case SIMULATE_EFFECT.BLACK_GREY_FILTER:
-				activeBlackAndGreyFilter();
-				break;
-			case SIMULATE_EFFECT.LIGHT_BLUR_FILTER:
-				activeLightBlurFilter();
-				break;
-			case SIMULATE_EFFECT.HEAVY_BLUR_FILTER:
-				activeHeavyBlurFilter();
-				break;
-			case SIMULATE_EFFECT.DRUNKEN_MOUSE:
-				activeDrunkenMouse();
-				break;
-			case SIMULATE_EFFECT.SCRAMBLE_LETTER:
-				activeScrambleLetter();
-				break;
-		}
-	});
+  effects.forEach((effect) => {
+    switch (effect) {
+      case SIMULATE_EFFECT.ZOOM:
+        activeFullZoom();
+        break;
+      case SIMULATE_EFFECT.HIGHT_CONTRAST_FILTER:
+        activeHighContrastFilter();
+        break;
+      case SIMULATE_EFFECT.BLACK_GREY_FILTER:
+        activeBlackAndGreyFilter();
+        break;
+      case SIMULATE_EFFECT.LIGHT_BLUR_FILTER:
+        activeLightBlurFilter();
+        break;
+      case SIMULATE_EFFECT.HEAVY_BLUR_FILTER:
+        activeHeavyBlurFilter();
+        break;
+      case SIMULATE_EFFECT.DRUNKEN_MOUSE:
+        activeDrunkenMouse();
+        break;
+      case SIMULATE_EFFECT.SCRAMBLE_LETTER:
+        activeScrambleLetter();
+        break;
+      case SIMULATE_EFFECT.DISTRACTIONS:
+        activeDistractions();
+        break;
+    }
+  });
 };
 
 export const removeSimulations = () => {
-	resetZoom();
-	removeColorFilter();
-	removeBlurFilter();
-	removeDrunkenMouse();
-	removeScrambleLetter();
+  resetZoom();
+  removeColorFilter();
+  removeBlurFilter();
+  removeDrunkenMouse();
+  removeScrambleLetter();
+  removeDistractions();
 };
