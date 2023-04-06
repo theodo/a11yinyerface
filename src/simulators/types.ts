1ba7622644666df52a3849ffb29ff38890ff4554
RobinCar <robinc@theodo.fr>
2023-04-06 11:49:09 +0200

    feat: add distractions simulator

--- src/simulators/types.ts
+++ src/simulators/types.ts
@@ -1,20 +1,21 @@
 export enum SIMULATE_EFFECT {
-	ZOOM = 'zoom',
-	HIGHT_CONTRAST_FILTER = 'hightContrastFilter',
-	BLACK_GREY_FILTER = 'blackGreyFilter',
-	LIGHT_BLUR_FILTER = 'lightBlurFilter',
-	HEAVY_BLUR_FILTER = 'heavyBlurFilter',
-	DRUNKEN_MOUSE = 'drunkenMouse',
-	SCRAMBLE_LETTER = 'scrambleLetter',
+  ZOOM = "zoom",
+  HIGHT_CONTRAST_FILTER = "hightContrastFilter",
+  BLACK_GREY_FILTER = "blackGreyFilter",
+  LIGHT_BLUR_FILTER = "lightBlurFilter",
+  HEAVY_BLUR_FILTER = "heavyBlurFilter",
+  DRUNKEN_MOUSE = "drunkenMouse",
+  SCRAMBLE_LETTER = "scrambleLetter",
+  DISTRACTIONS = "distractions",
 }
 
 export enum COLOR_FILTER_ID {
-	PROTANOPIA = 'protanopia', // All yellow
-	PROTANOMALY = 'protanomaly',
-	DEUTERANOPIA = 'deuteranopia',
-	DEUTERANOMALY = 'deuteranomaly',
-	TRITANOPIA = 'tritanopia',
-	TRITANOMALY = 'tritanomaly',
-	ACHROMATOPSIA = 'achromatopsia', // All grey
-	ACHROMATOMALY = 'achromatomaly',
+  PROTANOPIA = "protanopia", // All yellow
+  PROTANOMALY = "protanomaly",
+  DEUTERANOPIA = "deuteranopia",
+  DEUTERANOMALY = "deuteranomaly",
+  TRITANOPIA = "tritanopia",
+  TRITANOMALY = "tritanomaly",
+  ACHROMATOPSIA = "achromatopsia", // All grey
+  ACHROMATOMALY = "achromatomaly",
 }
