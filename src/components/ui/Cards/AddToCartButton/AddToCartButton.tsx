c58eee56170157a5ccd64833faa912cb63b731bc
RobinCar <robinc@theodo.fr>
2023-05-11 11:10:42 +0200

    feat: add item to card when click on button

--- src/components/ui/Cards/AddToCartButton/AddToCartButton.tsx
+++ src/components/ui/Cards/AddToCartButton/AddToCartButton.tsx
@@ -1,6 +1,8 @@
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import { IconButton } from "@mui/material";
+import { useAtom } from "jotai";
 
+import { cartAtom } from "src/store/cart";
 import { Product } from "src/types/product";
 
 export interface IAddToCartButton {
@@ -12,12 +14,22 @@ const AddToCartButton: React.FC<IAddToCartButton> = ({
   product,
   tiny = false,
 }) => {
+  const [cart, setCart] = useAtom(cartAtom);
+
+  const addToCart = () => {
+    const cartItem = cart[product.id] ?? { product, quantity: 0 };
+    cartItem.quantity += 1;
+    setCart((cart) => {
+      return { ...cart, [product.id]: cartItem };
+    });
+  };
+
   return (
     <IconButton
       aria-label="Add cart"
       color="inherit"
       style={tiny ? { padding: 0 } : {}}
-      onClick={() => alert("Congrats")}
+      onClick={addToCart}
     >
       <AddShoppingCartIcon style={tiny ? { fontSize: "12px" } : {}} />
     </IconButton>
