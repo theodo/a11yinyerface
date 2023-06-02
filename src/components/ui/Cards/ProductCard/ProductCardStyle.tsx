054ccfd3c0e58f20b0d4b412d5892b64d62c38c5
fsoussand <fflloo98@me.com>
2023-06-02 10:47:42 +0200

    redesign shopping cards

---
+++ src/components/ui/Cards/ProductCard/ProductCardStyle.tsx
@@ -0,0 +1,26 @@
+import { Product } from "src/types/product";
+
+export const stylePrice: React.CSSProperties = {
+  borderRadius: 50,
+  backgroundColor: "white",
+  fontSize: "12px",
+  display: "flex",
+  alignItems: "center",
+  padding: "5px",
+  border: "grey 1px solid",
+};
+
+const imageSize = 218;
+
+export const getImageStyle = (product: Product, tiny: boolean) => {
+  const style: React.CSSProperties = {
+    height: imageSize,
+    width: tiny ? "" : imageSize,
+    backgroundImage: `url(${product.previewImageUrl})`,
+    backgroundSize: "cover",
+    display: "flex",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  };
+  return style;
+};
