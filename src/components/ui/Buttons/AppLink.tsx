be4cd3f96761a8e8b7d9c48a903ed8e530fe20ba
fsoussand <fflloo98@me.com>
2023-06-22 09:21:38 +0200

    create generic link component

---
+++ src/components/ui/Buttons/AppLink.tsx
@@ -0,0 +1,37 @@
+import { Link } from "@mui/material";
+import Image from "next/image";
+
+interface IAppLink {
+  text: string;
+  link: string;
+}
+
+const AppLink: React.FC<IAppLink> = ({ text, link }) => {
+  return (
+    <Link
+      href={link}
+      sx={{
+        display: "flex",
+        width: "fit-content",
+        textTransform: "uppercase",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "75px",
+        backgroundColor: "primary.main",
+        ":hover": {
+          backgroundColor: "primary.dark",
+        },
+        alignSelf: "center",
+        gap: "10px",
+      }}
+    >
+      {text}
+      <Image
+        src={require("public/product_assets/images/white-arrow.svg")}
+        alt=""
+      />
+    </Link>
+  );
+};
+
+export default AppLink;
