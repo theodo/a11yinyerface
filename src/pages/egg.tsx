49bb141674e09542920661704a63e47bb20d5082
fsoussand <fflloo98@me.com>
2023-08-21 15:56:54 +0200

    translate egg page

--- src/pages/egg.tsx
+++ src/pages/egg.tsx
@@ -1,5 +1,6 @@
 import { CircularProgress } from "@mui/material";
 import type { NextPage } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import useSwr from "swr";
 
 import PageGridTemplate, {
@@ -8,6 +9,8 @@ import PageGridTemplate, {
 import { useSimulator } from "src/simulators";
 import { SIMULATE_EFFECT } from "src/simulators/types";
 import { Product } from "src/types/product";
+
+import { StaticProps } from "./_app";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -27,8 +30,17 @@ const EggPage: NextPage = () => {
       data={data}
       error={error}
       productCardType={ProductCardTypeEnum.Text}
+      translationFile="dairy_coffee_egg"
     />
   );
 };
 
 export default EggPage;
+
+export async function getStaticProps({ locale }: StaticProps) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["dairy_coffee_egg", "common"])),
+    },
+  };
+}
