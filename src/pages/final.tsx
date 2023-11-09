3c3c23f3fe5a1847ae1044be73aebb629ce3c700
external-RCZ <robinc@theodo.fr>
2023-11-09 17:30:09 +0100

    feature(final-page): add loader on final page

--- src/pages/final.tsx
+++ src/pages/final.tsx
@@ -1,8 +1,10 @@
+import { CircularProgress } from "@mui/material";
 import { Container } from "@mui/system";
 import { useAtom } from "jotai";
 import type { NextPage } from "next";
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useState } from "react";
 
 import AppLink from "src/components/ui/Buttons/AppLink";
 import FailureComponent from "src/components/ui/FinalPage/FailureComponent";
@@ -20,24 +22,62 @@ const FinalPage: NextPage = () => {
     "dairy_coffee_egg",
   ]);
   const [cart] = useAtom(cartAtom);
-  const expectedCart = [
-    { title: "apple", category: "fruits" },
-    { title: "egg", category: "dairy_coffee_egg" },
-    { title: "wheat", category: "flour" },
-  ];
-  const missingCart = [];
-  for (const expectedCartItem of expectedCart) {
-    if (
-      !Object.values(cart).find(
-        (cartItem) =>
-          cartItem.product.title === expectedCartItem.title + ".title"
-      )
-    ) {
-      missingCart.push(expectedCartItem);
+  const [isCartValidated, setIsCartValidated] = useState<boolean>(false);
+  const [missingCart, setMissingCart] = useState<
+    { title: string; category: string }[]
+  >([]);
+
+  useEffect(() => {
+    const expectedCart = [
+      { title: "apple", category: "fruits" },
+      { title: "egg", category: "dairy_coffee_egg" },
+      { title: "wheat", category: "flour" },
+    ];
+
+    const missingCartValidation = [];
+
+    for (const expectedCartItem of expectedCart) {
+      if (
+        !Object.values(cart).find(
+          (cartItem) =>
+            cartItem.product.title === expectedCartItem.title + ".title"
+        )
+      ) {
+        missingCartValidation.push(expectedCartItem);
+      }
     }
-  }
+    setMissingCart(missingCartValidation);
+
+    setIsCartValidated(true);
+  }, [cart]);
 
   const isCartValid = missingCart.length == 0;
+
+  const getContainerContent = () => {
+    if (!isCartValidated) {
+      return (
+        <Container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <CircularProgress />
+        </Container>
+      );
+    }
+    return (
+      <>
+        {isCartValid ? (
+          <SuccessComponent />
+        ) : (
+          <FailureComponent missingCart={missingCart} />
+        )}
+        <AppLink text={t("final-page.failure.try-again")} link="/" />
+      </>
+    );
+  };
+
   const content = (
     <Container
       sx={{
@@ -48,8 +88,7 @@ const FinalPage: NextPage = () => {
         rowGap: "2em",
       }}
     >
-      {isCartValid ? SuccessComponent() : FailureComponent({ missingCart })}
-      <AppLink text={t("final-page.failure.try-again")} link="/" />
+      {getContainerContent()}
     </Container>
   );
   return <PageTextTemplate content={content} />;
